6aaa74bc666b603d2743303cd042ea945521fb08
Esubalew Alemayehu Demissie <nattiy535@gmail.com>
2021-03-06 03:05:51 +0100

    Update CurrencyRepository.java

--- src/main/java/com/pharmapartners/opdracht/repository/CurrencyRepository.java
+++ src/main/java/com/pharmapartners/opdracht/repository/CurrencyRepository.java
@@ -1,10 +1,19 @@
+/**
+ * This is the repository to interact with Currencies from the database. It extends JpaRepository for CRUD methods and
+ * hence, uses JpaRepository’s methods: save(), findOne(), findById(), findAll(), count(), delete(), deleteById().
+ * In addition to these methods, I define custom finder methods. It is autowired in CurrencyController. The implementation
+ * is plugged in by Spring Data JPA automatically & currencies table will be generated in the H2 Database (in memory).
+ *
+ *  @author Esubalew A Demissie
+ *
+ */
+
 package com.pharmapartners.opdracht.repository;
 
 import com.pharmapartners.opdracht.model.Currency;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
