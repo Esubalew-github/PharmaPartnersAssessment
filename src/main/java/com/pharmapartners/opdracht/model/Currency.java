8a4c2a330d97218207124a09b2f128b2c8f4f149
Esubalew Alemayehu Demissie <nattiy535@gmail.com>
2021-03-09 11:12:47 +0100

    Update Currency.java

--- src/main/java/com/pharmapartners/opdracht/model/Currency.java
+++ src/main/java/com/pharmapartners/opdracht/model/Currency.java
@@ -15,7 +15,7 @@ import java.io.Serializable;
 public class Currency implements  Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "ticker")
